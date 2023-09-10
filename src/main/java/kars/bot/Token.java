b7388de854a9baad986f20e1361781cca9290eb8
Kars <97431994+Karscoding@users.noreply.github.com>
2023-09-10 22:53:40 +0200

    Token removed

--- src/main/java/kars/bot/Token.java
+++
@@ -1,7 +0,0 @@
-package kars.bot;
-
-public class Token {
-    public static String getToken() {
-        return "MTE0OTg2ODMzMjY0NDgyNzIxNg.GnQCQs.MC9zHcv30yUjhmPtl69ZUYZY6zmDURJcq9vXmg";
-    }
-}
